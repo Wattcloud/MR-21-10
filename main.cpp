f65a0abf5627e11c2e8f81ef7498323b512281e8
Awais Rana <rana.awais.bashir@gmail.com>
2024-10-10 09:51:36 +0500

    Signup function commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <signup.cpp>
 using namespace std;
 
 int main()
 {
     cout<<"Hello World"<<endl;
     cout<<"Second Line of Main Program"<<endl;
+    signup();
     return 0;
 }
